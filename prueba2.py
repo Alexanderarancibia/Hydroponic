ebf9d7e47df1cefda3b01faa887b1b9a3d8799dd
Alexanderarancibia <48413907+Alexanderarancibia@users.noreply.github.com>
2022-06-03 14:41:05 -0500

    Delete prueba2.py

--- prueba2.py
+++
@@ -1,6 +0,0 @@
-import json
-from datetime import datetime,date, time, timedelta
-# Opening JSON file
-f = open('parametros.json')
-data = json.load(f)
-print((data["Parametros_EC"][0])["Semana6_EC"][1])
