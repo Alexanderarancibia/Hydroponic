47809dd7c233548e73b2403f48d9ec355e148d24
Alexanderarancibia <48413907+Alexanderarancibia@users.noreply.github.com>
2022-06-03 14:41:26 -0500

    Delete shell.sh

--- shell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo “Hydroponic Code Setup”
-sudo cp services/hydro.service /lib/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable hydro.service
-sudo pip3 install -r requirements.txt
-sudo reboot
