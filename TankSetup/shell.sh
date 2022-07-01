35310ad95b854c04b6af464abc97bc2739a1c8cf
Alexanderarancibia <48413907+Alexanderarancibia@users.noreply.github.com>
2022-07-01 11:33:05 -0500

    Update shell.sh

--- TankSetup/shell.sh
+++ TankSetup/shell.sh
@@ -5,4 +5,5 @@ sudo chmod 664 /lib/systemd/system/hydro.service
 sudo systemctl daemon-reload
 sudo systemctl enable hydro.service
 sudo pip3 install -r requirements.txt
+sudo cat ../crontab.txt  |sudo crontab -
 sudo reboot
