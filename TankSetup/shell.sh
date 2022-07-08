f15261d8aaa0d7f22c6d20059626eaf2ca6f83a2
Alexanderarancibia <48413907+Alexanderarancibia@users.noreply.github.com>
2022-07-08 14:38:13 -0500

    Update shell.sh

--- TankSetup/shell.sh
+++ TankSetup/shell.sh
@@ -2,6 +2,7 @@
 echo “Hydroponic Code Setup”
 sudo cp hydro.service /lib/systemd/system/
 sudo chmod 664 /lib/systemd/system/hydro.service
+sudo chmod 777 /home/pi/Documents/Hydroponic/Tank_Module.py
 sudo systemctl daemon-reload
 sudo systemctl enable hydro.service
 sudo pip3 install -r requirements.txt
