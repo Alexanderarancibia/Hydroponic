c85fa6d4043ca692fe0830df0155a82b95e06512
Alexanderarancibia <48413907+Alexanderarancibia@users.noreply.github.com>
2022-07-09 11:31:03 -0500

    Update shell.sh

--- CO2Setup/shell.sh
+++ CO2Setup/shell.sh
@@ -2,6 +2,7 @@
 echo “Hydroponic Code Setup”
 sudo cp co2.service /lib/systemd/system/
 sudo chmod 664 /lib/systemd/system/co2.service
+sudo chmod 777 /home/pi/Documents/Hydroponic/CO2.py
 sudo systemctl daemon-reload
 sudo systemctl enable hydro.service
 sudo pip3 install -r requirements.txt
